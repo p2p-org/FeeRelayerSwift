eb422d2e13b956e0062ac05371331020eccc4c94
Chung Tran <bigearsenal@gmail.com>
2021-12-29 14:05:03 +0700

    feat: getUserRelayAddress

--- FeeRelayerSwift/Classes/Relay/FeeRelayer.Relay.Program.swift
+++ FeeRelayerSwift/Classes/Relay/FeeRelayer.Relay.Program.swift
@@ -6,11 +6,16 @@
 //
 
 import Foundation
+import SolanaSwift
 
 extension FeeRelayer.Relay {
     struct Program {
-        static let id = "24tpHRcbGKGYFGMYq66G3hfH8GQEYGTysXqiJyaCy9eR"
+        static let id: SolanaSDK.PublicKey = "24tpHRcbGKGYFGMYq66G3hfH8GQEYGTysXqiJyaCy9eR"
         
-        
+        static func getUserRelayAddress(
+            user: SolanaSDK.PublicKey
+        ) throws -> SolanaSDK.PublicKey {
+            try .createWithSeed(fromPublicKey: user, seed: "relay", programId: id)
+        }
     }
 }
