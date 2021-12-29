ba38548aefa7067958518bebb9e18d33c99af8e9
Chung Tran <bigearsenal@gmail.com>
2021-12-29 12:53:50 +0700

    feat: FeeRelayer.Relay.Program

---
+++ FeeRelayerSwift/Classes/Relay/FeeRelayer.Relay.Program.swift
@@ -0,0 +1,16 @@
+//
+//  FeeRelayer.Relay.Program.swift
+//  FeeRelayerSwift
+//
+//  Created by Chung Tran on 29/12/2021.
+//
+
+import Foundation
+
+extension FeeRelayer.Relay {
+    struct Program {
+        static let id = "24tpHRcbGKGYFGMYq66G3hfH8GQEYGTysXqiJyaCy9eR"
+        
+        
+    }
+}
