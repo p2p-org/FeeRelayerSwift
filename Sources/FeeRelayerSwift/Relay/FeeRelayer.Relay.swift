728f424b6cf14208add6e5dcc8f97af5854d1fea
Chung Tran <bigearsenal@gmail.com>
2022-02-28 17:25:39 +0700

    fix: top up min relay account balance

--- Sources/FeeRelayerSwift/Relay/FeeRelayer.Relay.swift
+++ Sources/FeeRelayerSwift/Relay/FeeRelayer.Relay.swift
@@ -196,35 +196,41 @@ extension FeeRelayer {
             if neededAmount.total > 0 {
                 return getRelayAccountStatus()
                     .map { [weak self] relayAccountStatus in
-                        guard let self = self else { return expectedFee }
-                        // TODO: - Unknown fee when first time using fee relayer
+                        guard let self = self,
+                              let minimumRelayAccountBalance = self.cache.minimumRelayAccountBalance
+                        else { return expectedFee }
+                        
+                        // Relay account has not been created
                         if relayAccountStatus == .notYetCreated {
-                            neededAmount.transaction += self.getRelayAccountCreationCost()
+                            // Relay account must keep minimum balance of minimumRelayAccountBalance to keeps address alive (es: 890880 lamports)
+                            neededAmount.transaction += minimumRelayAccountBalance
                         }
                         
-                        // Check account balance
-                        if var relayAccountBalance = relayAccountStatus.balance,
-                           relayAccountBalance > 0
-                        {
+                        // Relay account has already been created
+                        else if let relayAccountBalance = relayAccountStatus.balance {
                             // if relayAccountBalance has enough balance to cover transaction fee
                             if relayAccountBalance >= neededAmount.transaction {
                                 
-                                relayAccountBalance -= neededAmount.transaction
                                 neededAmount.transaction = 0
                                 
                                 // if relayAccountBlance has enough balance to cover accountBalances fee too
-                                if relayAccountBalance >= neededAmount.accountBalances {
+                                if relayAccountBalance - neededAmount.transaction >= neededAmount.accountBalances {
                                     neededAmount.accountBalances = 0
                                 }
                                 
                                 // Relay account balance can cover part of account creation fee
                                 else {
-                                    neededAmount.accountBalances -= relayAccountBalance
+                                    neededAmount.accountBalances -= (relayAccountBalance - neededAmount.transaction)
                                 }
                             }
                             // if not, relayAccountBalance can cover part of transaction fee
                             else {
                                 neededAmount.transaction -= relayAccountBalance
+                            }
+                            
+                            // if relayAccountBalance has less than min relay balance
+                            if relayAccountBalance < minimumRelayAccountBalance {
+                                neededAmount.transaction += minimumRelayAccountBalance - relayAccountBalance
                             }
                         }
                         return neededAmount
