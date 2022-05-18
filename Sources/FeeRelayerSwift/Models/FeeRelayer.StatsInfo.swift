e015e0e06b942e160591615771338c18fe3f01e9
Chung Tran <bigearsenal@gmail.com>
2022-05-18 08:21:18 +0700

    feat: StatsInfo struct

---
+++ Sources/FeeRelayerSwift/Models/FeeRelayer.StatsInfo.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+extension FeeRelayer {
+    struct StatsInfo: Encodable {
+        enum OperationType: String, Encodable {
+            case topUp = "TopUp"
+            case transfer = "Transfer"
+            case swap = "Swap"
+        }
+        
+        enum DeviceType: String, Encodable {
+            case web = "Web"
+            case android = "Android"
+            case iOS = "Ios"
+        }
+        
+        let operationType: OperationType
+        let deviceType: DeviceType
+        let currency: String?
+        let build: String
+        
+        enum CodingKeys: String, CodingKey {
+            case operationType = "operation_type"
+            case deviceType = "device_type"
+            case currency
+            case build
+        }
+    }
+}
