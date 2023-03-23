da5ea87b5f6a61f53486f744423a3de7ce690a90
Chung Tran <bigearsenal@gmail.com>
2023-03-23 11:45:04 +0700

    feat: some fixes

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
             targets: ["FeeRelayerSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/p2p-org/solana-swift.git", from: "2.1.1"),
-        .package(url: "https://github.com/p2p-org/OrcaSwapSwift.git", from: "2.1.1")
+        .package(url: "https://github.com/p2p-org/solana-swift.git", from: "3.0.0"),
+        .package(url: "https://github.com/p2p-org/OrcaSwapSwift.git", from: "3.0.0")
     ],
     targets: [
         .target(
